538bfd09d1e7c4f718e616566501bf0247703994
Pedro Tammela <pctammela@gmail.com>
2017-05-07 12:01:25 -0300

    fixup! Add tarefa7 part 2

--- tarefa7/tarefa2.lua
+++ tarefa7/tarefa2.lua
@@ -4,7 +4,6 @@ pin2 = 2
 led1 = 1
 lastinterrupt = tmr.now()
 idlastinterrupt = 0
-blinkmultiplier = 1.0
 timer = timer.create()
 speed = 1000 -- in ms
 
@@ -12,18 +11,12 @@ gpio.mode(pin1, gpio.INT)
 gpio.mode(pin2, gpio.INT)
 gpio.mode(led1, gpio.OUTPUT)
 
--- escopo lexico ( interrupcao 1 )
+-- escopo lexico ( interrupcao 1 ) + rapido
 do
    local function pincb(level, time)
       local lastpulse = time - lastinterrupt
-      if lastpulse <= 500 and idlastinterrupt ~= 1 then
-         if waspressed then
-            gpio.write(led1, gpio.LOW)
-         else
-            timer:alarm(speed - 50, tmr.ALARM_AUTO, function()
-               local state = gpio.read(led) == gpio.LOW and gpio.HIGH or gpio.LOW
-               gpio.write(led, state)
-            end)
+      if lastpulse <= 500 and idlastinterrupt ~= 1 and idlastinterrup ~= 0 then
+         gpio.write(led1, gpio.LOW)
       else
          timer:alarm(speed - 50, tmr.ALARM_AUTO, function()
                local state = gpio.read(led) == gpio.LOW and gpio.HIGH or gpio.LOW
@@ -37,20 +30,12 @@ do
    gpio.trig(pin1, "down", pincb)
 end
 
--- escopo lexico ( interrupcao 2 )
+-- escopo lexico ( interrupcao 2 ) + devagar
 do
    local function pincb(level, time)
       local lastpulse = time - lastinterrupt
-      if lastpulse <= 500 and idlastinterrup ~= 2 then
-         if waspressed then
+      if lastpulse <= 500 and idlastinterrupt ~= 2 and idlastinterrupt ~= 0 then
             gpio.write(led1, gpio.LOW)
-         else
-            timer:alarm(speed + 50, tmr.ALARM_AUTO, function()
-               local state = gpio.read(led) == gpio.LOW and gpio.HIGH or gpio.LOW
-               gpio.write(led, state)
-            end)
-
-         end
       else
          timer:alarm(speed + 50, tmr.ALARM_AUTO, function()
                local state = gpio.read(led) == gpio.LOW and gpio.HIGH or gpio.LOW
@@ -64,4 +49,7 @@ do
    gpio.trig(pin2, "down", pincb)
 end
 
-
+timer:alarm(speed, tmr.ALARM_AUTO, function()
+   local state = gpio.read(led) == gpio.LOW and gpio.HIGH or gpio.LOW
+   gpio.write(led, state)
+end)
