f3e5df89873cfa89083fa073b4770bb093a9505d
Bruna Aleixo <bruna.aleixo97@hotmail.com>
2017-05-08 21:51:11 -0300

    Edit tarefa 7 to increase and decrease blink speed upon button click

--- tarefa7/tarefa2.lua
+++ tarefa7/tarefa2.lua
@@ -1,31 +1,47 @@
--- verificar se os botões e leds correspondem a esses pinos
+-- verificar se os botões e led1s correspondem a esses pinos
 pin1 = 1
 pin2 = 2
-led1 = 1
-lastinterrupt = tmr.now()
+led1 = 3
+timepassed = 0
+lasttime = 0
 idlastinterrupt = 0
-timer = timer.create()
+timer = tmr.create()
 speed = 1000 -- in ms
 
-gpio.mode(pin1, gpio.INT)
-gpio.mode(pin2, gpio.INT)
 gpio.mode(led1, gpio.OUTPUT)
+gpio.mode(pin1,gpio.INT,gpio.PULLUP)
+gpio.mode(pin2, gpio.INT,gpio.PULLUP)
+
+function updateSpeed(pin, diff, timepassed)
+   speed = speed + diff
+
+   -- 1000000us = 1s
+   if timepassed <= 1000000 and idlastinterrupt ~= pin then
+      timer:stop()
+      gpio.write(led1, gpio.LOW)
+   else
+      timer:stop()
+      speed = speed <= 0 and 1000 or speed
+      timer:alarm(speed, tmr.ALARM_AUTO, function()
+         local state = gpio.read(led1) == gpio.LOW and gpio.HIGH or gpio.LOW
+         gpio.write(led1, state)
+      end)
+   end
+
+   idlastinterrupt = pin
+end
 
 -- escopo lexico ( interrupcao 1 ) + rapido
 do
    local function pincb(level, time)
-      local lastpulse = time - lastinterrupt
-      if lastpulse <= 500 and idlastinterrupt ~= 1 and idlastinterrup ~= 0 then
-         gpio.write(led1, gpio.LOW)
-      else
-         timer:alarm(speed - 50, tmr.ALARM_AUTO, function()
-               local state = gpio.read(led) == gpio.LOW and gpio.HIGH or gpio.LOW
-               gpio.write(led, state)
-            end)
-
-      end
-      lastinterrupt = tmr.now()
-      idlastinterrupt = 1
+      timepassed = time - lasttime
+      
+      print("pin1")
+      print(time..'-'..lasttime..'='..timepassed)
+      
+      lasttime = time
+      
+      updateSpeed(pin1, -100, timepassed)
    end
    gpio.trig(pin1, "down", pincb)
 end
@@ -33,23 +49,18 @@ end
 -- escopo lexico ( interrupcao 2 ) + devagar
 do
    local function pincb(level, time)
-      local lastpulse = time - lastinterrupt
-      if lastpulse <= 500 and idlastinterrupt ~= 2 and idlastinterrupt ~= 0 then
-            gpio.write(led1, gpio.LOW)
-      else
-         timer:alarm(speed + 50, tmr.ALARM_AUTO, function()
-               local state = gpio.read(led) == gpio.LOW and gpio.HIGH or gpio.LOW
-               gpio.write(led, state)
-            end)
+      timepassed = time - lasttime
+      
+      print("pin2")
+      print(time..'-'..lasttime..'='..timepassed)
 
-      end
-      lastinterrupt = tmr.now()
-      idlastinterrupt = 2
+      lasttime = time
+      updateSpeed(pin2, 100, timepassed)
    end
    gpio.trig(pin2, "down", pincb)
 end
 
 timer:alarm(speed, tmr.ALARM_AUTO, function()
-   local state = gpio.read(led) == gpio.LOW and gpio.HIGH or gpio.LOW
-   gpio.write(led, state)
+   local state = gpio.read(led1) == gpio.LOW and gpio.HIGH or gpio.LOW
+   gpio.write(led1, state)
 end)
