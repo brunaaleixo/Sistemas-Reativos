e41b4943a7f2d6b351419bb3bdda524848144f63
Bruna Aleixo <bruna.aleixo97@hotmail.com>
2017-05-28 19:44:48 -0300

    Clean up and comment code

--- mini_projeto3/main.lua
+++ mini_projeto3/main.lua
@@ -2,8 +2,8 @@ local x, y = love.graphics.getDimensions()
 
 function initializeCar()
    car = {
-     x = x/2 - carWidth/2,
-     y = y/2 - carHeight/2,
+     x = x/2 - carImage:getWidth()/2,
+     y = y/2 - carImage:getHeight()/2,
      xvel = 0,
      yvel = 0,
      rotation = 0
@@ -34,58 +34,64 @@ function initializeJoystick()
 end
 
 function love.update(dt)
-   local increment = joystick.acceleration == 0 and 0.2 or joystick.acceleration
-   ACCELERATION = ACCELERATION + increment > 10 and 10 or ACCELERATION + increment
+   if joystick.acceleration ~= 0 then
+    local increment = joystick.acceleration
+    ACCELERATION = ACCELERATION + increment > 10 and 10 or ACCELERATION + increment
+   end
 
-   print("dt", dt)
-   print("xvel", car.xvel)
-   print("yvel", car.yvel)
-   print("rotation", car.rotation)
+   -- Determina a velocidade no eixo x
+   car.xvel = car.xvel + ACCELERATION*dt * math.cos(car.rotation)
+   if (car.rotation < 0 and car.xvel > 0) or (car.rotation > 0 and car.xvel < 0) then
+      car.xvel = -car.xvel
+   end
 
+   -- Determina a posição no eixo x do carro
+   car.x = car.x + car.xvel*dt
+
+
+   -- Obtem a rotação do carro
    car.rotation = joystick.rotation
 
-   car.xvel = car.xvel + ACCELERATION*dt * math.cos(car.rotation)
+   -- Determina a velocidade do carro no eixo y baseado na sua rotação
    if (car.rotation == 0) then
      car.yvel = car.yvel + ACCELERATION*dt
    else
      car.yvel = car.yvel + ACCELERATION*dt * math.abs(math.sin(car.rotation))
    end
 
-   if (car.rotation < 0 and car.xvel > 0) or (car.rotation > 0 and car.xvel < 0) then
-      car.xvel = -car.xvel
-   end
-
-   car.x = car.x + car.xvel*dt
 
    car.xvel = car.xvel * 0.99
    car.yvel = car.yvel * 0.99
 
-   dy = math.abs(dy + car.yvel) > roadHeight and 0 or dy + car.yvel
+
+   -- Atualiza a posição do background de acordo com a velocidade no eixo y
+   dy = math.abs(dy + car.yvel) > road:getHeight() and 0 or dy + car.yvel
 
 end
 
 function love.draw()
+   -- Desenha uma imagem de uma estrada 3 vezes (posição das imagens é atualizada 
+   -- de acordo com a velocidade no eixo y para criar uma ilusão de movimento)
    love.graphics.draw(road, 0, dy)
    love.graphics.draw(road, 0, -y + dy)
    love.graphics.draw(road, 0, y + dy)
+
+   -- Desenha o carro
    love.graphics.draw(carImage, car.x, car.y, math.rad(car.rotation), 0.5)
 end
 
-
 function love.load()
-   ACCELERATION = 4
-   ANGACCEL = 4
-   inclination = 0
-   speed = 0
+   ACCELERATION = 2
    dy = 0
 
+   -- Carrega as imagens
    road = love.graphics.newImage("road.png")
    carImage = love.graphics.newImage("car.png")
 
-   roadHeight = road:getHeight()
-   carWidth = carImage:getWidth()
-   carHeight = carImage:getHeight()
+   -- Inicializa o carro no centro da tela
    initializeCar()
+
+   -- Inicializa a aceleração e a rotação do carro
    joystick = {
       acceleration = 0,
       rotation = 0,
